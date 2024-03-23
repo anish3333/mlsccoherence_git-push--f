83e8a897c4c8b9d86e6b4964cc773d35bacd1d26
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-03-23 19:34:08 +0530

    hamberger

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar/Sidebar'
-import DisplayCard from '../components/Card/DisplayCard'
-import Footer from '../components/Footer/Footer'
+
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar/Sidebar';
+import DisplayCard from '../components/Card/DisplayCard';
+import Footer from '../components/Footer/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Main() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+    // const openSidebar = () => {
+    //     setIsSidebarOpen(true);
+    // };
+
     return (
         <div className='flex'>
-            <Sidebar />
-            <div className='overflow-y-auto w-full'>
+            {isSidebarOpen ? (
+                <div className='fixed text-black bg-white rounded-full p-2 text-4xl ml-40 z-20' onClick={toggleSidebar}>
+                    ✖
+                </div>
+            ) : (
+                <div className='fixed text-white bg-black rounded-full p-2 text-4xl ml-6 z-20' onClick={toggleSidebar}>
+                    ≡
+                </div>
+            )}
 
+
+
+
+            <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+            <div className='overflow-y-auto w-full'>
                 <div className='flex flex-wrap'>
                     <DisplayCard />
                 </div>
                 <Footer />
-
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Main
+export default Main;
