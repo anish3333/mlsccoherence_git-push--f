dc04c74f36075f2a17da9bb1cf6cf52a82e3b9fb
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-03-23 11:59:04 +0530

    updated tailwind config file and addded signup signin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import Footer from "./components/Footer/Footer"
 import { Routes, Route } from "react-router-dom"
 import Sidebar from "./components/Sidebar/Sidebar"
+import Signup from "./components/Form/Signup"
+import Signin from "./components/Form/Signin"
 
 
 function App() {
@@ -9,10 +11,12 @@ function App() {
     <>
       {/* <div className="w-20 border-2 h-20">Test</div> */}
       <Routes>
-        <Route path="/" element={<Sidebar />}>
-          <Route path="/footer" element={<Footer />} />
-          <Route path="/sidebar" element={<Sidebar />} />
-        </Route>
+        <Route path="/" element={<Sidebar />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+
       </Routes>
     </>
   )
