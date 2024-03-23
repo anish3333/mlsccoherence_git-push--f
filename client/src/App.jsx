5e5adf0c2643fd003e685ec110297149cabb64fe
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-03-23 10:21:58 +0530

    fotter added in app.jsx and superior router dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import Footer from "./components/Footer/Footer"
+import { Routes, Route } from "react-router-dom"
 
 
 function App() {
 
   return (
     <>
-      <div className="w-20 border-2 h-20">Test</div>
+      {/* <div className="w-20 border-2 h-20">Test</div> */}
+      <Routes>
+        <Route path="/" element={<Footer />}>
+          <Route path="/footer" element={<Footer />} />
+        </Route>
+      </Routes>
     </>
   )
 }
