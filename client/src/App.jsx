748c66c4d5f0aa65696cb9c107793fb9558725f2
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-03-23 14:23:57 +0530

    modifided dialog , signup sign in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar"
 import Signup from "./components/Form/Signup"
 import Signin from "./components/Form/Signin"
 import Dialog1 from "./components/Form/Dialog1"
+import Hero from "./components/Hero/Hero"
 
 
 function App() {
@@ -13,11 +14,11 @@ function App() {
       {/* <div className="w-20 border-2 h-20">Test</div> */}
       <Routes>
         <Route path="/" element={<Sidebar />} />
+        <Route path="/home" element={<Hero />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-<<<<<<< Updated upstream
         <Route path="/dashboard" element={<Sidebar />} />
         <Route path="/checklists" element={<Sidebar />} />
         <Route path="/settings" element={<Sidebar />} />
@@ -25,12 +26,9 @@ function App() {
         <Route path="/blogs" element={<Sidebar />} />
         <Route path="/notifications" element={<Sidebar />} />
         <Route path="/themes" element={<Sidebar />} />
-
-=======
         <Route path="/dialog" element={<Dialog1 />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
->>>>>>> Stashed changes
 
       </Routes>
     </>
